7e5bb140aa1b61cfee1df034a205fdeed3b4cf30
fullstack412 <dt897867@gmail.com>
2018-09-24 00:43:36 +0800

    Fix typo

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 import gallery from './gallery';
 
-const combinedReducer = combinedReducers({
+const combinedReducer = combineReducers({
     gallery
 });
 
